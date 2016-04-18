b014b82b72ec80442346087149fb167807dc3352
MatteoDome <mdomenighini@gmail.com>
2016-04-18 14:17:52 +0200

    mysteries of computer science. now everything works.

--- splitop.py
+++ splitop.py
@@ -3,20 +3,20 @@ from numba import jit
 import math
 import matplotlib.pyplot as plt
 
-n = 1000
+n = 2000
 T = 5*n
 t = 40
-t_up = 50
+t_up = 25
 dx = 1
-v0 = 1
+v0 = 2
 V = np.zeros(n)
-p = 1
+p = 0
 x = 100
 K = p**2 / 2
 X = np.linspace(0, n, n, dtype = 'float64')
 x = X/5
-sigma = 1
-psi = np.zeros(n, dtype = 'float64')
+sigma = np.sqrt(0.5)
+# psi = np.zeros(n, dtype = 'float64')
 
 def gaussian(x, t, sigma, n):
 	num = (x-t)**2
@@ -32,26 +32,28 @@ def barrier(n, v0, V):
 	return V
 
 def well(n, v0, V):
-	V = -barrier(n, V)
+	V = -barrier(n, v0, V)
 	return V
+
+def wave_func(x, t, sigma, n):
+	psi = gaussian (x, t, sigma, n)
+	psi = psi / np.sqrt(np.sum(psi*psi))
+	print(np.sum(psi*psi))
+	return psi
 
 def update (psi, V, p):
 	psi = np.fft.fft(psi*np.exp(-V*math.pi/2), norm = "ortho")
 	psi = np.fft.ifft(psi*np.exp(-p**2*math.pi/2), norm = "ortho")
 	return psi
 
-def wave_func(psi, x, t, sigma, n):
-	psi = gaussian (x, t, sigma, n)
-	psi2 = np.zeros
-	den = np.sqrt(np.sum(psi2))
-	psi = psi/den
-	return psi
+V = barrier(n, v0, V)
 
-for t in range (0,round(T/5)):
-	psi = wave_func(psi, x, t, sigma, n)
+for t in range (40, int(round(max(x)))):
+	psi = wave_func(x, t, sigma, n)
 	psi = update (psi, V, p)
 	if t % t_up == 0:
 		plt.plot(x, psi)
-		plt.hold()
+		plt.hold(True)
 
 plt.show()
+	
