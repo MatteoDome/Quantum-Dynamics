395bcac60a90d8528efd44be0c0edade5743f511
MatteoDome <mdomenighini@gmail.com>
2016-04-20 10:17:21 +0200

    added split operator starting from real space -- still not working

--- splitop.py
+++ splitop.py
@@ -3,64 +3,49 @@ from numba import jit
 import math
 import matplotlib.pyplot as plt
 
-n = 2000
-T = 5*n
-t = 20
-t_up = 25
-v0 = 3
-dt = 0.001
-V = np.zeros(n)
-p = 10
-alpha = 0.1
-x0 = 10
-X = np.linspace(-n, n, n, dtype = 'float64')
-x = X/5
-# psi = np.zeros(n, dtype = 'float64')
+maxP = 1000
+numpoints = 10*maxP
+p0 = 100
+p = np.linspace(0, maxP, numpoints)
+v0 = 0.00001
+dt = 0.1
+t_p = numpoints/5
+V = np.zeros(maxP*10)
+alpha =0.001
 
-def gaussian(p, t, alpha, n):
-	num = (x-x0)**2
-	return np.exp(-alpha*num)
-
-def step(n, v0, V):
-	V[n/2:] = v0
-	return V
-
-def barrier(n, v0, V):
-	V[n/3: 2*n/3] = v0
-	return V
-
-def well(n, v0, V):
-	V = -barrier(n, v0, V)
-	return V
-
-def wave_func(x, t, sigma, n):
-	psi = gaussian (x, t, sigma, n)
+def gaussian(p, p0):
+	psi = np.exp(-alpha*(p-p0)**2)
 	psi = psi / np.sqrt(np.sum(psi*psi))
-	psi_r = np.cos(x)*psi
-	psi_i = np.sin(x)*psi
-	return psi, psi_r, psi_i
+	return psi
 
-def update (psi, psi_r, psi_i, V, p):
-	psi = np.fft.fft(psi*np.exp(-dt*V*math.pi*2), norm = "ortho")
-	psi = np.fft.ifft(psi*np.exp(-dt*math.pi*p**2), norm = "ortho")
-	psi_r = np.fft.fft(psi_r*np.exp(-dt*V*math.pi*2), norm = "ortho")
-	psi_r = np.fft.ifft(psi_r*np.exp(-dt*p**2*math.pi), norm = "ortho")
-	psi_i = np.fft.fft(psi_i*np.exp(-dt*V*math.pi*2), norm = "ortho")
-	psi_i = np.fft.ifft(psi_i*np.exp(-dt*p**2*math.pi), norm = "ortho")
-	return psi, psi_r, psi_i
+def step(numpoints, v0, V):
+	V[numpoints/2:] = v0
+	return V
 
-#####################################################################
-########################  MAIN  #####################################
-#####################################################################
+def barrier(numpoints, v0, V):
+	V[numpoints/3: 2*numpoints/3] = v0
+	return V
+
+def well(numpoints, v0, V):
+	V = -barrier(numpoints, v0, V)
+	return V
+
+def update(psi, p, V, dt):
+	psi = np.fft.fft(psi*np.exp(-dt*math.pi*p**2), norm = "ortho")
+	psi = np.fft.ifft(psi*np.exp(-dt*V*math.pi*2), norm = "ortho")
+	return psi
 
 
-V = well(n, v0, V)
+plt.plot(p, V)
+plt.hold(True)
 
-for t in range (40, int(round(max(x)))):
-	psi, psi_r, psi_i = wave_func(x, t, alpha, n)
-	psi, psi_r, psi_i = update (psi, psi_r, psi_i, V, p)
-	if t % t_up == 0:
-		plt.plot(x, psi)
-		plt.show()
+psi = gaussian (p, p0)
 
-# plt.show()
+for t in range (0, 8*maxP):
+	psi = np.fft.fft(psi*np.exp(-dt*math.pi*p**2), norm = "ortho")
+	if t%t_p == 0:
+		plt.plot(p, np.imag(psi))
+		plt.hold(True)
+	psi = np.fft.ifft(psi*np.exp(-dt*V*math.pi*2), norm = "ortho")
+
+plt.show()
